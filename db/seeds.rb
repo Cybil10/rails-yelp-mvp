9496bb51bff37e82a8f720d442ff941c3d3907a0
Cybil LECHIGUERO <cybil.lechiguero@gmail.com>
2019-05-22 18:52:25 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  { name: 'Dishoom', address: '7 Boundary StLondon E2 7JE', phone_number: '01 60 33 34 33', category: 'french'},
+  { name: 'Pizza East', address: '56A Shoreditch London E1 6PQ', phone_number: '01 22 33 34 22', category: 'french'}
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+review = Review.new(content: "comment", rating: 5)
+review.restaurant = Restaurant.last
+review.save
+
